51f4c7b79444f5fea47155cefd0646abbb156465
Anna <anna@annamx.com>
2024-11-06 17:12:57 -0700

    configured the motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingBobbaRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingBobbaRobot.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@TeleOp (name = "BabbaTotalyCode!")
 public class DrivingBobbaRobot extends LinearOpMode {
 
     private DcMotor frontRightMotorB0;
@@ -17,6 +20,19 @@ public class DrivingBobbaRobot extends LinearOpMode {
         frontLeftMotorB1 = hardwareMap.get(DcMotor.class, "fl1");
         backRightMotorB2 = hardwareMap.get(DcMotor.class, "br2");
         backLeftMotorB3 = hardwareMap.get(DcMotor.class, "bl3");
+
+        backLeftMotorB3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotorB2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftMotorB1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotorB0.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        backLeftMotorB3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotorB2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeftMotorB1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotorB0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        frontLeftMotorB1.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotorB3.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Telemetry
         telemetry.addData("Status", "Initialized");
