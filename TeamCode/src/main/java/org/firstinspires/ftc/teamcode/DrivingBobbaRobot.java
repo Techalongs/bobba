4e64f31c81f98f888d8db0a4c8982f6c0c7f85b0
Anna <anna@annamx.com>
2024-11-10 11:03:41 -0700

    changed some names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingBobbaRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivingBobbaRobot.java
@@ -5,46 +5,50 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp (name = "BabbaTotalyCode!")
+@TeleOp(name = "BobbaTotalyCode!")
 public class DrivingBobbaRobot extends LinearOpMode {
 
-    private DcMotor frontRightMotorB0;
-    private DcMotor frontLeftMotorB1;
-    private DcMotor backRightMotorB2;
-    private DcMotor backLeftMotorB3;
+    private DcMotor motorFrontRightB0;
+    private DcMotor motorFrontLeftB1;
+    private DcMotor motorBackRightB2;
+    private DcMotor motorBackLeftB3;
 
     @Override
     public void runOpMode() throws InterruptedException {
         // Motors
-        frontRightMotorB0 = hardwareMap.get(DcMotor.class, "fr0");
-        frontLeftMotorB1 = hardwareMap.get(DcMotor.class, "fl1");
-        backRightMotorB2 = hardwareMap.get(DcMotor.class, "br2");
-        backLeftMotorB3 = hardwareMap.get(DcMotor.class, "bl3");
+        motorFrontRightB0 = hardwareMap.get(DcMotor.class, "fr0");
+        motorFrontLeftB1 = hardwareMap.get(DcMotor.class, "fl1");
+        motorBackRightB2 = hardwareMap.get(DcMotor.class, "br2");
+        motorBackLeftB3 = hardwareMap.get(DcMotor.class, "bl3");
 
-        backLeftMotorB3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRightMotorB2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontLeftMotorB1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontRightMotorB0.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackLeftB3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackRightB2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontLeftB1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontRightB0.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        backLeftMotorB3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRightMotorB2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontLeftMotorB1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRightMotorB0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackLeftB3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackRightB2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontLeftB1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontRightB0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        frontLeftMotorB1.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeftMotorB3.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFrontLeftB1.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackLeftB3.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Telemetry
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+        updateTelemetry("Status", "Initialized");
         waitForStart();
 
         while (opModeIsActive()) {
-            frontRightMotorB0.setPower(-gamepad1.left_stick_y-gamepad1.right_stick_x);
-            frontLeftMotorB1.setPower(-gamepad1.left_stick_y+gamepad1.right_stick_x);
-            backRightMotorB2.setPower(-gamepad1.left_stick_y-gamepad1.right_stick_x);
-            backLeftMotorB3.setPower(-gamepad1.left_stick_y+gamepad1.right_stick_x);
+            motorFrontRightB0.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x);
+            motorFrontLeftB1.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x);
+            motorBackRightB2.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x);
+            motorBackLeftB3.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x);
         }
+    }
+
+    public void updateTelemetry(String caption, Object value) {
+        telemetry.addData(caption, value);
+        telemetry.update();
     }
 
 }
